55b169ee92ae8744be642aa44e82e97d60531f55
Vaelot <vaelot@kakao.com>
2020-07-25 04:57:00 +0900

    Add a FXML file.

--- src/main/kotlin/xyz/vaelot/dupeflattener/view/MainView.kt
+++ src/main/kotlin/xyz/vaelot/dupeflattener/view/MainView.kt
@@ -1,10 +1,9 @@
 package xyz.vaelot.dupeflattener.view
 
+import javafx.scene.layout.BorderPane
+
 import tornadofx.*
 
 class MainView: View() {
-    override val root = vbox {
-        button("Press me")
-        label("Waiting")
-    }
+    override val root : BorderPane by fxml("/fxml/MainView.fxml")
 }
